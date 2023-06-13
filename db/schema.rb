d8818779c80b4599f74cde4c0d4f9c57ada6dd65
Angelos Makris <sade-1995@hotmail.com>
2023-06-13 18:07:44 +0300

    merging db:migrate fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema[7.0].define(version: 2023_06_13_143840) do
-=======
-ActiveRecord::Schema[7.0].define(version: 2023_06_13_134932) do
->>>>>>> master
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
