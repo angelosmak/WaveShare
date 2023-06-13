b0e4e35e2d7b35f83c002beceeddfd6b32e1ebd5
Thoren Depner <thorendepner@gmail.com>
2023-06-13 16:36:26 +0200

    seeds edited

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
-Review.destroy_all
-UserEvent.destroy_all
-Event.destroy_all
-Beach.destroy_all
-User.destroy_all
+# Review.destroy_all
+# UserEvent.destroy_all
+# Event.destroy_all
+# Beach.destroy_all
+# User.destroy_all
 #This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
