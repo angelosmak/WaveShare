571310f1ac6598225b9d83d2e017f7fdbe3490d2
IAntoine <131304683+IAntoine@users.noreply.github.com>
2023-06-14 08:05:25 +0200

    index and show ok

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,59 @@
 class EventsController < ApplicationController
+  before_action :set_event, only: [:show, :edit, :update, :destroy]
+
+  # GET /events
+  def index
+    @events = Event.all
+  end
+
+  # GET /events/:id
+  def show
+    # The event is already fetched by the set_event method
+  end
+
+  # GET /events/new
+  def new
+    @event = Event.new
+  end
+
+  # POST /events
+  def create
+    @event = Event.new(event_params)
+
+    if @event.save
+      redirect_to @event, notice: 'Event was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  # GET /events/:id/edit
+  def edit
+    # The event is already fetched by the set_event method
+  end
+
+  # PATCH/PUT /events/:id
+  def update
+    if @event.update(event_params)
+      redirect_to @event, notice: 'Event was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  # DELETE /events/:id
+  def destroy
+    @event.destroy
+    redirect_to events_url, notice: 'Event was successfully destroyed.'
+  end
+
+  private
+
+  def set_event
+    @event = Event.find(params[:id])
+  end
+
+  def event_params
+    params.require(:event).permit(:beach_id, :user_id, :title, :date, :description)
+  end
 end
