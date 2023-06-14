6afef40eb6f62f414754d49e71b07b9af0507e74
IAntoine <131304683+IAntoine@users.noreply.github.com>
2023-06-14 11:51:54 +0200

    events are onnnn

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,6 @@
 class EventsController < ApplicationController
   before_action :set_event, only: [:show, :edit, :update, :destroy]
-
+  before_action :set_beach, only: [:new, :create]
   def index
     @events = Event.all
   end
@@ -9,21 +9,20 @@ class EventsController < ApplicationController
   end
 
   def new
+    @beach = Beach.find(params[:beach_id])
     @event = Event.new
   end
 
 
   def create
-    @beach = Beach.find(params[:beach_id])
-
     @event = Event.new(event_params)
     @event.beach = @beach
     @event.user_id = current_user.id
 
     if @event.save
-      redirect_to @event, notice: 'Event was successfully created.'
+      redirect_to beach_path(@event.beach), notice: 'Event was successfully created.'
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
 
@@ -36,6 +35,10 @@ class EventsController < ApplicationController
 
   def set_event
     @event = Event.find(params[:id])
+  end
+
+  def set_beach
+    @beach = Beach.find(params[:beach_id])
   end
 
   def event_params
