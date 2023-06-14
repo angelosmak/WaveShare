70bc77b26f148e3ca00838863feb8f140c7571ea
Murdo Blythe <murdo.blythe@gmail.com>
2023-06-14 17:06:19 +0100

    Created 'Create' method for user-events controller

--- app/controllers/user_events_controller.rb
+++ app/controllers/user_events_controller.rb
@@ -1,2 +1,22 @@
 class UserEventsController < ApplicationController
+
+  def create
+    @attendance = UserEvent.new
+    @event = Event.find(params[:event_id])
+    @attendance.event = @event
+    @attendance.user_id = current_user.id
+    @attendance.save
+    redirect_to @event.beach, notice: 'Attendendance added successfully.'
+  end
+
+  def destroy
+    current_user.events.delete(@user_event)
+    redirect_to @user_event.event, notice: 'Attendance cancelled successfully.'
+  end
+
+  private
+
+  def set_beach
+    @beach = Beach.find(params[:beach_id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,14 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :profiles
   resources :reviews , except: [:new, :create, :edit, :update]
-  resources :events , except: [:new, :create]
+  resources :events , except: [:new, :create] do
+    resources :user_events, only:[:new, :create]
+  end
   resources :user_events, except: [:new, :create]
 
   resources :beaches do
     resources :reviews, only:[:new, :create]
     resources :events, only:[:new, :create]
-    resources :user_events, only:[:new, :create]
   end
   # Defines the root path route ("/")
   # root "articles#index"
