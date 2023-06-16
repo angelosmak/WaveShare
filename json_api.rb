186083027a015cfba4636de79d76e44d0bd07f0a
Thoren Depner <thorendepner@gmail.com>
2023-06-16 12:25:58 +0200

    api working with several dynamic datapoints

--- json_api.rb
+++ json_api.rb
@@ -3,8 +3,6 @@ require "json"
 
 # bondi beach
 api_key = 'ea186841ff8ba1d952efa9a35dfa1bed'
-lat = '-33.8919444444'
-lon = '151.2730555556'
 
 # url = "https://api.openweathermap.org/data/3.0/onecall?lat=#{lat}&lon=#{lon}&units==metric&appid=#{api_key}"
 
@@ -13,7 +11,3 @@ url = "https://api.openweathermap.org/data/2.5/weather?lat=#{lat}&lon=#{lon}&uni
 weather = URI.open(url).read
 
 surf_forecast = JSON.parse(weather)
-
-# p surf_forecast
-
-p surf_forecast["wind"]
