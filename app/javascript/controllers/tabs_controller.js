0985de541b14409b0a28a9126e60823248266aea
Thoren Depner <thorendepner@gmail.com>
2023-06-20 12:38:35 +0200

    tabs controller updated

---
+++ app/javascript/controllers/tabs_controller.js
@@ -0,0 +1,22 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="tabs"
+export default class extends Controller {
+  connect() {
+    console.log("Hello, Stimulus!")
+  }
+  openCity(event) {
+    console.log(event)
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+}
