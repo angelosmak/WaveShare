f81781f0a05cc2b9b2e7a2ee7bdbf8e5ec3bfabf
Angelos Makris <sade-1995@hotmail.com>
2023-06-22 11:30:18 +0300

    map working on events showpage

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -3,7 +3,8 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static values = {
     apiKey: String,
-    markers: Array
+    markers: Array,
+
 
   }
 
