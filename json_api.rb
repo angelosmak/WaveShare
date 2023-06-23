1eda6fde9e998efbdfdeac1d5d25deaa84244d6d
Angelos Makris <sade-1995@hotmail.com>
2023-06-23 12:51:42 +0300

    api member key

--- json_api.rb
+++ json_api.rb
@@ -5,8 +5,8 @@ require "json"
 api_key = '60e223f4f961a95134c63cb32ba647e3'
 
 # url = "https://api.openweathermap.org/data/3.0/onecall?lat=#{lat}&lon=#{lon}&units==metric&appid=#{api_key}"
-
-url = "https://api.openweathermap.org/data/2.5/weather?lat=#{lat}&lon=#{lon}&units==metric&appid=#{api_key}"
+url = "https://api.openweathermap.org/data/3.0/onecall?lat=#{lat}&lon=#{lon}&appid=#{api_key}"
+# url = "https://api.openweathermap.org/data/2.5/weather?lat=#{lat}&lon=#{lon}&units==metric&appid=#{api_key}"
 
 weather = URI.open(url).read
 
