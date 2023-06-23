76e1ddc56beb56a7bdf53fe91f99af66be52ea50
Angelos Makris <sade-1995@hotmail.com>
2023-06-23 12:39:58 +0300

    api key

--- json_api.rb
+++ json_api.rb
@@ -2,7 +2,7 @@ require "open-uri"
 require "json"
 
 # bondi beach
-api_key = 'd4e1a1852e538a510ed264567f3c800a'
+api_key = 'a613b838c3896d5d763d1733738491f7'
 
 # url = "https://api.openweathermap.org/data/3.0/onecall?lat=#{lat}&lon=#{lon}&units==metric&appid=#{api_key}"
 
