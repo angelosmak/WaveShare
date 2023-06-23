cdf8c9ca287f4b13d44ee354ec074d030c723565
Angelos Makris <sade-1995@hotmail.com>
2023-06-23 14:20:32 +0300

    weather api restore

--- app/javascript/controllers/beach_weather_controller.js
+++ app/javascript/controllers/beach_weather_controller.js
@@ -5,18 +5,20 @@ export default class extends Controller {
   static values = { lat: String, lon: String}
   static targets = ["description", "temperature", "Windspeed", "Winddirection", "Sunset", "icon"]
   connect() {
+
     console.log(this.latValue, this.lonValue)
     fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${this.latValue}&lon=${this.lonValue}&appid=60e223f4f961a95134c63cb32ba647e3`)
     .then(response => response.json())
     .then(data => {
+
       console.log(data)
-      this.descriptionTarget.insertAdjacentHTML("beforeend", data.weather[0].description)
-      this.temperatureTarget.insertAdjacentHTML("beforeend", `${Math.round(data.main.temp-273.15)}`)
+      this.descriptionTarget.insertAdjacentHTML("beforeend", data.current.weather[0].description)
+      this.temperatureTarget.insertAdjacentHTML("beforeend", `${Math.round(data.daily[0].temp.day - 273.15)}`)
       const directions = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW']
-      const windDir = directions[Math.round(data.wind.deg / 45) % 8]
-      this.WindspeedTarget.insertAdjacentHTML("beforeend", ` ${data.wind.speed} m/s`)
+      const windDir = directions[Math.round(data.current.wind_deg / 45) % 8]
+      this.WindspeedTarget.insertAdjacentHTML("beforeend", ` ${data.current.wind_speed} m/s`)
       this.WinddirectionTarget.insertAdjacentHTML("beforeend", `${windDir}`)
-      this.iconTarget.src=`https://openweathermap.org/img/w/${data.weather[0].icon}.png`
-      this.SunsetTarget.insertAdjacentHTML("beforeend", ` ${new Date(data.sys.sunset * 1000).toLocaleTimeString('en-GB', { hour: '2-digit', minute: '2-digit' })}`)
+      this.iconTarget.src=`https://openweathermap.org/img/w/${data.current.weather[0].icon}.png`
+      this.SunsetTarget.insertAdjacentHTML("beforeend", ` ${new Date(data.current.sunset * 1000).toLocaleTimeString('en-GB', { hour: '2-digit', minute: '2-digit' })}`)
     })
 }}
