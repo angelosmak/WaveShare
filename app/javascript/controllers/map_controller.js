4a612d704df5a6f4bb23e9e1e38ab99dc85ab7cf
Angelos Makris <sade-1995@hotmail.com>
2023-06-28 14:10:36 +0300

    mapbox

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -22,7 +22,7 @@ export default class extends Controller {
   #addMarkersToMap() {
     this.markersValue.forEach((marker) => {
       const popup = new mapboxgl.Popup().setHTML(marker.info_window_html)
-      new mapboxgl.()
+      new mapboxgl.Marker()
         .setLngLat([ marker.lng, marker.lat ])
         .setPopup(popup)
         .addTo(this.map)
