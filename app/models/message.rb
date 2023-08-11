851a41d6fa0654697aaa00bb26804f44467e9cca
Angelos Makris <sade-1995@hotmail.com>
2023-08-11 09:37:56 +0300

    created the model messages for the threads section

---
+++ app/models/message.rb
@@ -0,0 +1,6 @@
+class Message < ApplicationRecord
+  belongs_to :user
+  belongs_to :beach
+  has_many :replies, class_name: 'Message', foreign_key: 'parent_id', dependent: :destroy
+  belongs_to :parent, class_name: 'Message', optional: true
+end
